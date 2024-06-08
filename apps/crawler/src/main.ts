b4264cf0c501d46edc998a7609bfcd4514a0a693
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-06-08 16:56:26 +0900

    경로 설정

--- apps/crawler/src/main.ts
+++ apps/crawler/src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { CrawlerModule } from './crawler.module';
-
+import { AllExceptionsFilter } from '@libs/common/filter/exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(CrawlerModule);
+  const httpAdapter = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   await app.listen(3000);
+  //d
 }
 bootstrap();
