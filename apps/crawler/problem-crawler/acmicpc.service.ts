c18198b7f126bc450b00879dde2b674ddfc858f7
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-06-10 20:20:04 +0900

    feat: add boj

--- apps/crawler/problem-crawler/acmicpc.service.ts
+++ apps/crawler/problem-crawler/acmicpc.service.ts
@@ -1,55 +1,157 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { ProblemCralwer } from './problem-crawler.interface';
 import { CralwerCookieDto } from '@libs/common/dto/CrawlerCookieDto';
 import { HttpService } from '@nestjs/axios';
 import { ResponseProblemDto } from '@libs/common/dto/ResponseProblemDto';
 import { catchError, firstValueFrom } from 'rxjs';
 import { parse } from 'node-html-parser';
+import { ResponseProblemSummaryDto } from '@libs/common/dto/ResponseProblemSummaryDto';
 
 @Injectable()
 export class AcmicpcService implements ProblemCralwer {
+  private readonly tierToTextMap = {
+    '1': '브론즈 5',
+    '2': '브론즈 4',
+    '3': '브론즈 3',
+    '4': '브론즈 2',
+    '5': '브론즈 1',
+    '6': '실버 5',
+    '7': '실버 4',
+    '8': '실버 3',
+    '9': '실버 2',
+    '10': '실버 1',
+    '11': '골드 5',
+    '12': '골드 4',
+    '13': '골드 3',
+    '14': '골드 2',
+    '15': '골드 1',
+    '16': '플래티넘 5',
+    '17': '플래티넘 4',
+    '18': '플래티넘 3',
+    '19': '플래티넘 2',
+    '20': '플래티넘 1',
+    '21': '다이아몬드 5',
+    '22': '다이아몬드 4',
+    '23': '다이아몬드 3',
+    '24': '다이아몬드 2',
+    '25': '다이아몬드 1',
+    '26': '루비 5',
+    '27': '루비 4',
+    '28': '루비 3',
+    '29': '루비 2',
+    '30': '루비 1',
+  };
+  private readonly requestHeaders = {
+    accept:
+      'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+    'cache-control': 'no-cache',
+    pragma: 'no-cache',
+    priority: 'u=0, i',
+    'sec-ch-ua':
+      '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    referrer: 'https://www.acmicpc.net/',
+    referrerPolicy: 'strict-origin',
+    'User-Agent':
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0',
+  };
   constructor(private readonly httpService: HttpService) {}
 
-  async getProblemList(cookies?: CralwerCookieDto[]) {
+  async getProblemList(
+    startPageNo: number,
+    endPageNo: number,
+    cookies?: CralwerCookieDto[],
+  ) {
     const requestCookies = cookies ?? [];
+    const headers = { ...this.requestHeaders };
+    const requestUrl = 'https://www.acmicpc.net/problemset';
 
-    return [] as ResponseProblemDto[];
+    let currentPage = startPageNo;
+    const result = [];
+
+    while (currentPage <= endPageNo) {
+      const response = await firstValueFrom(
+        this.httpService
+          .get(`${requestUrl}/${currentPage}`, {
+            headers,
+          })
+          .pipe(
+            catchError((error) => {
+              const status = Number(error.response.status);
+
+              if (status == HttpStatus.NOT_FOUND) {
+                throw new NotFoundException('can not find problem');
+              }
+
+              console.error(error);
+
+              throw new InternalServerErrorException(error.message);
+            }),
+          ),
+      );
+
+      const data = response.data;
+      const summrayList = this.parseProblemList(data);
+      summrayList.forEach((elem) => result.push(elem));
+
+      currentPage++;
+    }
+
+    return result;
   }
+
+  parseProblemList(data: string) {
+    return [
+      ...parse(data)
+        .querySelector('#problemset')
+        .querySelector('tbody')
+        .querySelectorAll('tr'),
+    ].map((elem) => {
+      const title = elem.querySelectorAll('td')[1].querySelector('a').innerText;
+      const key = elem.querySelector('.list_problem_id').innerText;
+      const tierImage = elem.querySelectorAll('td')[1].querySelector('img')
+        ?.attributes['src'];
+
+      console.log(tierImage);
+      return {
+        title,
+        key,
+      };
+    });
+  }
+
   async getProblem(key: string, cookies?: CralwerCookieDto[]) {
     const requestCookies = cookies ?? [];
     const requestUrl = `https://www.acmicpc.net/problem/${key}`;
-    console.log(requestUrl);
-    const requestHeaders = {
-      accept:
-        'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-      'accept-language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
-      'cache-control': 'no-cache',
-      pragma: 'no-cache',
-      priority: 'u=0, i',
-      'sec-ch-ua':
-        '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
-      'sec-ch-ua-mobile': '?0',
-      'sec-ch-ua-platform': '"macOS"',
-      'sec-fetch-dest': 'document',
-      'sec-fetch-mode': 'navigate',
-      'sec-fetch-site': 'same-origin',
-      'sec-fetch-user': '?1',
-      'upgrade-insecure-requests': '1',
-      referrer: 'https://www.acmicpc.net/',
-      referrerPolicy: 'strict-origin',
-      'User-Agent':
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0',
-    };
-
+    const headers = { ...this.requestHeaders };
     const response = await firstValueFrom(
       this.httpService
         .get(requestUrl, {
-          headers: requestHeaders,
+          headers,
         })
         .pipe(
           catchError((error) => {
-            console.log('error');
+            const status = Number(error.response.status);
+
+            if (status == HttpStatus.NOT_FOUND) {
+              throw new NotFoundException('can not find problem');
+            }
+
+            console.error(error);
+
             throw new InternalServerErrorException(error.message);
           }),
         ),
@@ -57,13 +159,14 @@ export class AcmicpcService implements ProblemCralwer {
 
     const data = response.data;
     const responseProblemDto = this.parseProblem(data);
+    responseProblemDto.key = key;
     return responseProblemDto;
   }
 
   parseProblem(data: string): ResponseProblemDto {
     const document = parse(data);
     const title = document.querySelector('#problem_title').text;
-    console.log(`title: ${title}`);
+    // console.log(`title: ${title}`);
 
     const contentList = document
       .querySelector('#problem_description')
@@ -76,7 +179,7 @@ export class AcmicpcService implements ProblemCralwer {
       .filter((elem) => elem)
       .join('\n');
 
-    console.log(`입력 : ${input}`);
+    // console.log(`입력 : ${input}`);
 
     const output = document
       .querySelector('#problem_output')
@@ -84,7 +187,7 @@ export class AcmicpcService implements ProblemCralwer {
       .filter((elem) => elem)
       .join('\n');
 
-    console.log(`출력 : ${output}`);
+    // console.log(`출력 : ${output}`);
 
     const problemInfoList = document
       .querySelector('#problem-info')
@@ -92,7 +195,7 @@ export class AcmicpcService implements ProblemCralwer {
       .querySelector('tr')
       .querySelectorAll('td')
       .map((elem) => elem.innerHTML);
-    console.log(problemInfoList);
+    // console.log(problemInfoList);
 
     const timeout = +problemInfoList[0].split(' ')[0].replace(/[^0-9]/g, '');
     const memoryLimit = +problemInfoList[1].replace(/[^0-9]/g, '');
@@ -108,7 +211,7 @@ export class AcmicpcService implements ProblemCralwer {
         ?.filter((elem) => elem)
         ?.join('\n') ?? '';
 
-    console.log(`제한 : ${limit}`);
+    // console.log(`제한 : ${limit}`);
 
     const inputOutputList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
       .map((elem) => {
@@ -133,12 +236,13 @@ export class AcmicpcService implements ProblemCralwer {
       .filter((elem) => elem.input && elem.output);
 
     // console.log(inputOutputNodeList);
-    console.log(contentList);
+    // console.log(contentList);
 
     const responseProblemDto = new ResponseProblemDto();
     responseProblemDto.title = title;
 
     return {
+      key: '',
       title,
       contentList,
       level: '',
