ba951c5b91f20f7af074eab5b5aa442339b38bd7
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-06-19 19:02:34 +0900

    feat: add validationSchema

--- apps/algogo/src/app.module.ts
+++ apps/algogo/src/app.module.ts
@@ -2,14 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { validationSchema } from './config/validationSchema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
       isGlobal: true,
+      validationSchema,
     }),
   ],
+
   controllers: [AppController],
   providers: [AppService],
 })
