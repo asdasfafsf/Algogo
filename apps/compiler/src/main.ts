5176c460a20991d0f5a0bbc0234c43eefb5a5c77
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-06-21 20:02:09 +0900

    feat: 공통 코드 정리

--- apps/compiler/src/main.ts
+++ apps/compiler/src/main.ts
@@ -3,6 +3,9 @@ import { CompilerModule } from './compiler.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(CompilerModule);
+  import { AllExceptionsFilter } from '@libs/filter/src/index';
+  import { ResponseInterceptor } from '@libs/interceptor/src/index';
+
   await app.listen(3000);
 }
 bootstrap();
