7ceaa3ef618509d283ef82daa81faa3879555e04
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-08-15 23:25:05 +0900

    feat: python 에러 처리방식 변경

--- apps/compiler/src/execute/python-interpret.service.ts
+++ apps/compiler/src/execute/python-interpret.service.ts
@@ -30,38 +30,36 @@ export class PythonInterpretService extends InterpretService {
   }
 
   handleError(error: Error): void {
-    if (error instanceof RuntimeError) {
-      if (error.message.includes('RecursionError:')) {
-        error.message = 'RecursionError';
-      } else if (error.message.includes('ValueError:')) {
-        error.message = 'ValueError';
-      } else if (error.message.includes('IndexError:')) {
-        error.message = 'IndexError';
-      } else if (error.message.includes('NameError:')) {
-        error.message = 'NameError';
-      } else if (error.message.includes('TypeError:')) {
-        error.message = 'TypeError';
-      } else if (error.message.includes('AssertionError:')) {
-        error.message = 'AssertionError';
-      } else if (error.message.includes('FileNotFoundError:')) {
-        error.message = 'FileNotFoundError';
-      } else if (error.message.includes('SyntaxError:')) {
-        error.message = 'SyntaxError';
-      } else if (error.message.includes('AttributeError:')) {
-        error.message = 'AttributeError';
-      } else if (error.message.includes('ZeroDivisionError:')) {
-        error.message = 'ZeroDivisionError';
-      } else if (error.message.includes('ModuleNotFoundError:')) {
-        error.message = 'ModuleNotFoundError';
-      } else if (error.message.includes('UnboundLocalError:')) {
-        error.message = 'UnboundLocalError';
-      } else if (error.message.includes('OverflowError:')) {
-        error.message = 'OverflowError';
-      } else if (error.message.includes('EOFError:')) {
-        error.message = 'EOFError';
-      }
+    if (error.message.includes('RecursionError:')) {
+      throw new RuntimeError('RecursionError');
+    } else if (error.message.includes('ValueError:')) {
+      throw new RuntimeError('ValueError');
+    } else if (error.message.includes('IndexError:')) {
+      throw new RuntimeError('IndexError');
+    } else if (error.message.includes('NameError:')) {
+      throw new RuntimeError('NameError');
+    } else if (error.message.includes('TypeError:')) {
+      throw new RuntimeError('TypeError');
+    } else if (error.message.includes('AssertionError:')) {
+      throw new RuntimeError('AssertionError');
+    } else if (error.message.includes('FileNotFoundError:')) {
+      throw new RuntimeError('FileNotFoundError');
+    } else if (error.message.includes('SyntaxError:')) {
+      throw new RuntimeError('SyntaxError');
+    } else if (error.message.includes('AttributeError:')) {
+      throw new RuntimeError('AttributeError');
+    } else if (error.message.includes('ZeroDivisionError:')) {
+      throw new RuntimeError('ZeroDivisionError');
+    } else if (error.message.includes('ModuleNotFoundError:')) {
+      throw new RuntimeError('ModuleNotFoundError');
+    } else if (error.message.includes('UnboundLocalError:')) {
+      throw new RuntimeError('UnboundLocalError');
+    } else if (error.message.includes('OverflowError:')) {
+      throw new RuntimeError('OverflowError');
+    } else if (error.message.includes('EOFError:')) {
+      throw new RuntimeError('EOFError');
     }
 
-    throw error;
+    throw new RuntimeError('');
   }
 }
