cca856e0b766ceeb89898981aae42e8bfa11e78c
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-09-01 22:14:11 +0900

    etc: remove unused

--- apps/algogo/src/execute/execute.gateway.ts
+++ apps/algogo/src/execute/execute.gateway.ts
@@ -2,26 +2,13 @@ import {
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
-  WebSocketServer,
 } from '@nestjs/websockets';
 import { ExecuteService } from './execute.service';
 import { RequestExecuteDto } from '@libs/core/dto/RequestExecuteDto';
-import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
 export class ExecuteGateway {
   constructor(private readonly executeService: ExecuteService) {}
-
-  @WebSocketServer()
-  server: Server;
-
-  afterInit(server: Server) {
-    console.log('server init');
-  }
-  handleConnection(client: Socket) {
-    console.log('handle connection');
-  }
-  handleDisconnect(client: Socket) {}
 
   @SubscribeMessage('execute')
   async handleMessage(@MessageBody() requestExecuteDto: RequestExecuteDto) {
