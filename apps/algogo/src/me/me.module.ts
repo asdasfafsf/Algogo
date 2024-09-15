06cad015b6e2f838ce67bff117e761a16177e70c
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-09-15 23:57:30 +0900

    feat: add dockerfile

--- apps/algogo/src/me/me.module.ts
+++ apps/algogo/src/me/me.module.ts
@@ -5,21 +5,13 @@ import { MeRepository } from './me.repository';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
 import { S3Module } from '../s3/s3.module';
-import { FileModule } from 'apps/compiler/src/file/file.module';
 import { CryptoModule } from '../crypto/crypto.module';
 import { ImageModule } from '../image/image.module';
 
 @Module({
   controllers: [MeController],
   providers: [MeService, MeRepository],
-  imports: [
-    PrismaModule,
-    AuthModule,
-    S3Module,
-    FileModule,
-    CryptoModule,
-    ImageModule,
-  ],
+  imports: [PrismaModule, AuthModule, S3Module, CryptoModule, ImageModule],
   exports: [],
 })
 export class MeModule {}
