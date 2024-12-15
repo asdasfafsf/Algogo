b5269bc690855e13f2d5dc1db670f86da6119779
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-12-15 18:53:00 +0900

    feat: add oauthDto

---
+++ apps/algogo/src/oauth/dto/RequestOAuthDto.ts
@@ -0,0 +1,32 @@
+import {
+  IsString,
+  IsOptional,
+  IsIn,
+  IsEmail,
+  IsNotEmpty,
+} from 'class-validator';
+
+export class RequestOAuthDto {
+  @IsIn(['google', 'github', 'kakao'])
+  @IsNotEmpty()
+  provider: 'google' | 'github' | 'kakao';
+
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  accessToken: string;
+
+  userNo?: number;
+  ip?: string;
+}
