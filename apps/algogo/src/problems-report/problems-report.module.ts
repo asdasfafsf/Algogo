ba138bd303eb0b267e69e47f7b64c667ae2a9adf
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2024-12-31 01:31:57 +0900

    feat:

--- apps/algogo/src/problems-report/problems-report.module.ts
+++ apps/algogo/src/problems-report/problems-report.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProblemsReportService } from './problems-report.service';
 import { ProblemsReportController } from './problems-report.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   controllers: [ProblemsReportController],
   providers: [ProblemsReportService],
+  imports: [AuthModule],
 })
 export class ProblemsReportModule {}
