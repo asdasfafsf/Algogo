c74c8055bfe827efac3bba71c0aa443b02803371
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2025-01-26 22:28:42 +0900

    feat: add customException

---
+++ apps/algogo/src/common/errors/CustomConflictException.ts
@@ -0,0 +1,8 @@
+import { HttpStatus } from '@nestjs/common';
+import CustomHttpException from './CustomHttpException';
+
+export class CustomConflictException extends CustomHttpException {
+    constructor(error: CustomError) {
+        super(error, HttpStatus.CONFLICT);
+    }
+}
