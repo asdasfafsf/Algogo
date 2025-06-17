c914d1176a6e48d23ad1e0b96c8caf51e80757b3
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-06-17 22:46:49 +0900

    fix: fix type

--- apps/algogo/src/me/dto/ResponseOAuthDto.ts
+++ apps/algogo/src/me/dto/ResponseOAuthDto.ts
@@ -1,10 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { OAuthProvider } from '../../common/enums/OAuthProviderEnum';
+import { OAuthProvider } from '../../oauth-v2/types/oauth.type';
+import { OAUTH_PROVIDER } from '../../oauth-v2/constants/oauth.contant';
 
 export class ResponseOAuthDto {
   @ApiProperty({
     description: 'OAuth기관',
-    enum: OAuthProvider,
+    enum: OAUTH_PROVIDER,
   })
   provider: OAuthProvider;
 }
