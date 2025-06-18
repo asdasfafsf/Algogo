a25d014bd617a3720c05b86221b3476123199e86
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2025-06-19 03:31:25 +0900

    fix: 토큰 추출 수정

--- apps/algogo/src/auth-v2/auth-refresh.guard.ts
+++ apps/algogo/src/auth-v2/auth-refresh.guard.ts
@@ -1,5 +1,7 @@
 import { ExecutionContext, Injectable, CanActivate } from '@nestjs/common';
 import { JwtService } from '../jwt/jwt.service';
+import { JwtMissingTokenException } from '../common/errors/token/JwtMissingTokenException';
+import { Request } from 'express';
 
 @Injectable()
 export class AuthRefreshGuard implements CanActivate {
@@ -7,16 +9,31 @@ export class AuthRefreshGuard implements CanActivate {
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    const token = this.extractToken(request);
+    const token = this.extractTokenFromHeader(request);
 
-    if (!token) return false;
+    if (!token) {
+      throw new JwtMissingTokenException();
+    }
 
-    const payload = await this.jwtService.verify(token); // 이미 다중 시크릿 지원
+    const payload = await this.jwtService.verify(token); 
     request.user = { ...payload, refreshToken: token };
     return true;
   }
 
-  private extractToken(request: any): string | undefined {
-    return request.headers.authorization?.split(' ')[1];
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [typeFromHeader, tokenFromHeader] =
+      request?.headers['authorization']?.split(' ') ?? [];
+    if (typeFromHeader === 'Bearer' && tokenFromHeader) {
+      return tokenFromHeader;
+    }
+
+    const [typeFromCookie, tokenFromCookie] =
+      request?.cookies?.authorization?.split(' ') ?? [];
+    if (typeFromCookie === 'Bearer' && tokenFromCookie) {
+      return tokenFromCookie;
+    }
+
+    return undefined;
   }
 }
