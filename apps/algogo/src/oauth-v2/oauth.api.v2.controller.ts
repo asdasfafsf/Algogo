9f0adcd6e3ce419931e4f820e64c58389530f33e
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2025-06-19 16:00:31 +0900

    feat: req.user => req.oauth

--- apps/algogo/src/oauth-v2/oauth.api.v2.controller.ts
+++ apps/algogo/src/oauth-v2/oauth.api.v2.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { DynamicOAuthGuard } from './dynamic-oauth.guard';
 import { OAuthRequest } from './types/oauth.type';
 import { OauthV2Service } from './oauth-v2.service';
+import { AuthV2Guard } from '../auth-v2/auth-v2.guard';
 
 @Controller('api/v2/oauth')
 export class OauthApiV2Controller {
@@ -10,13 +11,25 @@ export class OauthApiV2Controller {
   @Get('/:provider')
   @UseGuards(DynamicOAuthGuard)
   async oauth(@Req() req: OAuthRequest) {
-    const { user } = req;
+    const { oauth } = req;
     const { accessToken, refreshToken } =
-      await this.oauthV2Service.registerOrLogin(user);
+      await this.oauthV2Service.registerOrLogin(oauth);
 
     return {
       accessToken,
       refreshToken,
     };
   }
+
+  // @Post('/connect/:provider')
+  // @UseGuards(AuthV2Guard, DynamicOAuthGuard)
+  // async connectOAuth(@Req() req: OAuthRequest) {
+  //   const { user } = req;
+  //   const { provider } = req.params;
+  //   await this.oauthV2Service.connectOAuthProvider({
+  //     id: user.id,
+  //     provider,
+  //     userUuid: user.uuid,
+  //   });
+  // }
 }
