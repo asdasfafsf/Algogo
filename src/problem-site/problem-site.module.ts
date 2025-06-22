17e775872b83ba1646b912641505a5d318aa188c
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2025-06-23 02:53:37 +0900

    feat: problem-site api 임시 개발 완료

--- src/problem-site/problem-site.module.ts
+++ src/problem-site/problem-site.module.ts
@@ -3,10 +3,12 @@ import { ProblemSiteController } from './problem-site.controller';
 import { ProblemSiteService } from './problem-site.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthGuardModule } from '../auth-guard/auth-guard.module';
+import { ProblemSiteRepository } from './problem-site.repository';
+import { AuthorizationModule } from '../authorization/authorization.module';
 
 @Module({
   controllers: [ProblemSiteController],
-  providers: [ProblemSiteService],
-  imports: [PrismaModule, AuthGuardModule],
+  providers: [ProblemSiteService, ProblemSiteRepository],
+  imports: [PrismaModule, AuthGuardModule, AuthorizationModule],
 })
 export class ProblemSiteModule {}
