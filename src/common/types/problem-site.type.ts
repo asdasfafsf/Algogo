05b19169f0aa69719d23175c1f7244c73c0b0712
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2025-06-23 02:42:42 +0900

    fix: fix import

--- src/common/types/problem-site.type.ts
+++ src/common/types/problem-site.type.ts
@@ -1,4 +1,4 @@
-import { PROBLEM_SITE_PROVIDER } from '../constants/external.constant';
+import { PROBLEM_SITE_PROVIDER } from '../constants/problem-site.constant';
 
 export type ProblemSiteProvider =
   (typeof PROBLEM_SITE_PROVIDER)[keyof typeof PROBLEM_SITE_PROVIDER];
