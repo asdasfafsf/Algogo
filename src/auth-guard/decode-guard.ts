2d664a0bb386c1853e946731e3739d6a41cd2fc4
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2025-06-24 03:30:19 +0900

    fix: 인증이 필요 없는 경우 가드. 추가

---
+++ src/auth-guard/decode-guard.ts
@@ -0,0 +1,43 @@
+import { ExecutionContext, Injectable, CanActivate } from '@nestjs/common';
+import { JwtService } from '../jwt/jwt.service';
+import { Request } from 'express';
+
+@Injectable()
+export class DecodeGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      request.user = null;
+      return true;
+    }
+
+    try {
+      const payload = await this.jwtService.verify(token);
+      request.user = payload;
+    } catch (error) {
+      request.user = null;
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [typeFromHeader, tokenFromHeader] =
+      request?.headers['authorization']?.split(' ') ?? [];
+    if (typeFromHeader === 'Bearer' && tokenFromHeader) {
+      return tokenFromHeader;
+    }
+
+    const [typeFromCookie, tokenFromCookie] =
+      request?.cookies?.authorization?.split(' ') ?? [];
+    if (typeFromCookie === 'Bearer' && tokenFromCookie) {
+      return tokenFromCookie;
+    }
+
+    return undefined;
+  }
+}
